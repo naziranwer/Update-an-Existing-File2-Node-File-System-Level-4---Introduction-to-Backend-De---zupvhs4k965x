3f8a06afa7c3c0c2c27b3fe6b2ba4770c2af8cf2
MOHD NAZIR ANWER <122814116+naziranwer@users.noreply.github.com>
2023-12-29 12:54:45 +0530

    create

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ const fileName = 'myfile.txt';
 const updateFile = async (fileName, fileContent) => {
     //Write your code here to overwrite the file content
     //Don't change function name
+
+    try{
+
+        const fileExists = await fs.access(fileName)
+            .then(() => true)
+            .catch(() => false);
+
+        if(fileExists){
+            const existingContents=await fs.readFile(fileName,"utf8");
+
+            const updateContents= existingContents+ fileContent;
+
+            await fs.writeFile(fileName, updateContents);
+        }
+    }
+    catch(e){
+        console.error(e);
+    }
     
 };
 
